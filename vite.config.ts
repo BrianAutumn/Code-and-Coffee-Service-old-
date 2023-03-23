44b51fa5c71cdc61281f08256510ba2b73250c8f
BJTowne <bjtowne9@gmail.com>
2023-03-22 22:05:42 -0400

    Fixing build

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig({
+  define: {
+    'process.env': {NODE_ENV:'production'}
+  },
   build: {
     lib: {
       entry: resolve(__dirname, "src/web/index.tsx"),
