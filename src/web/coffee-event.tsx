e94a72d35d0bff64a7dbfe84a43b2dc5ca3bee44
BJTowne <bjtowne9@gmail.com>
2023-03-23 18:54:21 -0400

    Fixing calendar

--- src/web/coffee-event.tsx
+++ src/web/coffee-event.tsx
@@ -242,9 +242,7 @@ export function CoffeeEvent({event}:{event:MeetupEvent}) {
   }
 
   useEffect(() => {
-    console.log('fetching icon')
     fetch(`/city-icons/${event.group.id}.png`).then(response => {
-      console.log('loading complete')
       if (response.ok) {
         setIconImage(<CityIcon src={`/city-icons/${event.group.id}.png`} alt={`${event.group.city} Icon`}/>)
         setSmallIconImage(<SmallCityIcon src={`/city-icons/${event.group.id}.png`} alt={`${event.group.city} Icon`}/>)
