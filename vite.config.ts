a318694157bb81dad26d3146b5f949a9c6cc4b34
BJTowne <bjtowne9@gmail.com>
2023-03-22 21:43:23 -0400

    Fixing build

--- vite.config.ts
+++ vite.config.ts
@@ -12,11 +12,20 @@ export default defineConfig({
       name:'Index',
       fileName: "index",
     },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        }
+      }
+    },
   },
   resolve:{
     alias:{
-      'react': 'https://unpkg.com/react@18/umd/react.production.min.js',
-      'react-dom': 'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js'
+      'react':'https://esm.sh/react@18.2.0',
+      'react-dom':'https://esm.sh/react-dom@18.2.0'
     }
   }
 });
