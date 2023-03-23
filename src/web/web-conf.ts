823c871306cb95e7f66af99b587856365de2f391
BJTowne <bjtowne9@gmail.com>
2023-03-23 19:14:29 -0400

    Added root URL

--- src/web/web-conf.ts
+++ src/web/web-conf.ts
@@ -1,4 +1,4 @@
 export const WebConf = {
   smBreakpoint: 600,
-  rootUrl:import.meta.env.VITE_ROOT_URL || 'https://d3ldp4mijqfzeg.cloudfront.net/',
+  rootUrl:'https://d3ldp4mijqfzeg.cloudfront.net/',
 }
